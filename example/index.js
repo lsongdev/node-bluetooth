ef248747056a9c8c07f8c25db4cf5b8ebb84230c
Lsong <song940@gmail.com>
2016-09-29 12:10:34 +0800

    modify readme

--- example/index.js
+++ example/index.js
@@ -1,27 +1,25 @@
 const bluetooth = require('../');
 
+// create bluetooth device instance
 const device = new bluetooth.DeviceINQ();
 
-device.on('found', function(address, name){
+var list = [];
 
+device
+.on('finished',  console.log.bind(console, 'finished'))
+.on('found', function found(address, name){
+  
   console.log('Found: ' + address + ' with name ' + name);
-
-  device.findSerialPortChannel(address, function(channel){
+  
+  // find serial port channel
+  device.findSerialPortChannel(dev.address, function(channel){
+    console.log('Found RFCOMM channel for serial port on %s: ', dev.name, channel);
     
-    console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+    // make bluetooth connect to remote device
+    bluetooth.connect(dev.address, channel, function(connection){
+      connection.write(new Buffer('Hello!', 'utf-8'));
+    });
     
-    // bluetooth.connect(address, channel, function(err, connection){
-    //   connection.write('hi');
-    // });
   });
-});
-
-device.on('finished', function(){
-  console.log('scan finished.');
-});
-
-device.listPairedDevices(function(devices){
-  console.log(devices);
-});
-
-device.inquire();
+  
+}).inquire();
