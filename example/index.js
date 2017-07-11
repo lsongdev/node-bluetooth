f2d67e3f3c105ae949fc73b4523a32492387c63a
Lsong <song940@gmail.com>
2017-07-11 11:34:45 +0800

    modify example and readme

--- example/index.js
+++ example/index.js
@@ -8,19 +8,24 @@ var list = [];
 device
 .on('finished',  console.log.bind(console, 'finished'))
 .on('found', function found(address, name){
-  
+
   console.log('Found: ' + address + ' with name ' + name);
-  
+
   // find serial port channel
   device.findSerialPortChannel(address, function(channel){
     console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
-    
+
     // make bluetooth connect to remote device
     bluetooth.connect(address, channel, function(err, connection){
       if(err) return console.error(err);
+
+      connection.on('data', (buffer) => {
+        console.log('received message:', buffer.toString());
+      });
+
       connection.write(new Buffer('Hello!', 'utf-8'));
     });
-    
+
   });
-  
+
 }).inquire();
